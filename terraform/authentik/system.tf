a379874bacd60c6167ad80be949e85f8eb780a31
Alex Vegas <v34l@proton.me>
2026-01-20 18:59:30 -0500

    adding terraform module for authentik

---
+++ terraform/authentik/system.tf
@@ -0,0 +1,29 @@
+# Set Brand config
+resource "authentik_brand" "authentik-test" {
+  domain         = "."
+  default        = true
+  branding_title = "CEDILLE-test"
+  branding_favicon = "/static/dist/assets/icons/icon.png"
+  branding_logo = "/media/custom/branding/Logo_text_white.png"
+  flow_authentication = "default-authentication-flow" # (String)
+  flow_invalidation = "default-invalidation-flow" # (String)
+  flow_user_settings = "default-user-settings-flow" # (String)
+  attributes = jsonencode({
+    settings = {
+      theme = {
+        base = "dark"
+      },
+      locale = "fr_CA"
+    }
+  })
+
+  # branding_default_flow_background = "" # (String) Defaults to /static/dist/assets/images/flow_background.jpg.
+  # branding_custom_css = "" # (String)
+  # client_certificates = "" # (List of String)
+  # default_application = "" # (String)
+  # flow_device_code = "" # (String)
+  # flow_recovery = "" # (String)
+  # flow_unenrollment = "" # (String)
+  # web_certificate = "" # (String)
+
+}
