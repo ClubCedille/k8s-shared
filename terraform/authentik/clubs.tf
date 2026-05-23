b2086dd872505d6b7dbe9de44c937d398aae718a
Alex Vegas <v34l@proton.me>
2026-05-22 20:42:30 -0400

    add applets

--- terraform/authentik/clubs.tf
+++ terraform/authentik/clubs.tf
@@ -1,6 +1,6 @@
-module "clubs" {
-  source = "./modules/groups"
+# module "clubs" {
+#   source = "./modules/groups"
 
-  for_each = var.clubs
-  name     = each.value
-}
+#   for_each = var.clubs
+#   name     = each.value
+# }
