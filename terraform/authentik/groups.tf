71b9f26987a69f62545bdd5e4423de0f5e5bdbc7
Alex <67081273+alexvegas22@users.noreply.github.com>
2026-09-15 23:25:07 -0400

    Feature/authentik terraform (#468)
    * test-tarraform group créé
    
    * existing club
    
    * existing club added for test
    
    * terraform import authentik
    
    * move import to root
    
    * add club cedille role
    
    * fix role import
    
    * add resource
    
    * reference authentik module
    
    * use role id
    
    * typo rbac role
    
    * remove importr
    
    * fix club-cedille role import
    
    ---------
    
    Co-authored-by: Kyohkotsu <fenrir200791@gmail.com>

--- terraform/authentik/groups.tf
+++ terraform/authentik/groups.tf
@@ -2,6 +2,23 @@ resource "authentik_group" "test-terraform" {
   name = "test-terraform"
 }
 
+#resource "authentik_group" "admin" {
+#  name = "admin"
+#  is_superuser = false
+#}
+
 resource "authentik_group" "club-cedille" {
   name = "club-cedille"
+  is_superuser = false
+  roles = [
+    authentik_rbac_role.club-cedille.id
+  ]
 }
+
+resource "authentik_rbac_role" "club-cedille" {
+  name = "club-cedille"
+}
+#resource "authentik_group" "exec-cedille" {
+#  name = "exec-cedille"
+#  is_superuser = false
+#}
